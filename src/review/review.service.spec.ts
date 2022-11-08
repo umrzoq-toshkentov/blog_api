b9d7449a2f667fd31524cb0f1a1c26ddd9a9a003
UmrzoqScope3 <utoshkentov@scope3.com>
2022-11-08 16:31:38 +0500

    feat: unit and e2e tests

--- src/review/review.service.spec.ts
+++ src/review/review.service.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Types } from 'mongoose';
+import { getModelToken } from 'nestjs-typegoose';
 import { ReviewService } from './review.service';
 
 describe('ReviewService', () => {
   let service: ReviewService;
+  const exec = { exec: jest.fn() };
+
+  const reviewRepositorFactory = () => ({
+    find: () => exec,
+  });
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ReviewService],
+      providers: [
+        ReviewService,
+        {
+          useFactory: reviewRepositorFactory,
+          provide: getModelToken('ReviewModel'),
+        },
+      ],
     }).compile();
 
     service = module.get<ReviewService>(ReviewService);
@@ -14,5 +27,14 @@ describe('ReviewService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('findByProductId working', async () => {
+    const id = new Types.ObjectId().toHexString();
+    reviewRepositorFactory()
+      .find()
+      .exec.mockReturnValueOnce([{ productId: id }]);
+    const res = await service.findByProductId(id);
+    expect(res[0].productId).toBe(id);
   });
 });
---
+++ test/review.e2e-spec.ts
@@ -0,0 +1,77 @@
+import { CreateReviewDto } from './../src/review/dto/create-review.dto';
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../src/app.module';
+import { Types, disconnect } from 'mongoose';
+
+const productId = new Types.ObjectId().toHexString();
+const testDto: CreateReviewDto = {
+  name: 'Umrzoq',
+  title: 'test title',
+  description: 'new description',
+  rating: 5,
+  productId,
+};
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication;
+  let createdId: string;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/review/create (POST) - success', async () => {
+    return request(app.getHttpServer())
+      .post('/review/create')
+      .send(testDto)
+      .expect(201)
+      .then(({ body }: request.Response) => {
+        createdId = body._id;
+        expect(createdId).toBeDefined();
+      });
+  });
+
+  it('/review/byProduct/:productId (GET) - success', async () => {
+    return request(app.getHttpServer())
+      .get('/review/byProduct/' + productId)
+      .expect(200)
+      .then(({ body }: request.Response) => {
+        expect(body.length).toBe(1);
+      });
+  });
+
+  it('/review/byProduct/:productId (GET) - fail', async () => {
+    return request(app.getHttpServer())
+      .get('/review/byProduct/' + new Types.ObjectId().toHexString())
+      .expect(200)
+      .then(({ body }: request.Response) => {
+        expect(body.length).toBe(0);
+      });
+  });
+
+  it('/review/:id (DELETE) - success', () => {
+    return request(app.getHttpServer())
+      .delete('/review/' + createdId)
+      .expect(200);
+  });
+
+  it('/review/:id (DELETE) - fail', () => {
+    return request(app.getHttpServer())
+      .delete('/review/' + new Types.ObjectId().toHexString())
+      .expect(404, {
+        statusCode: 404,
+        message: 'Review with this id not found',
+      });
+  });
+
+  afterAll(() => {
+    disconnect();
+  });
+});
